ec13a4217144ad7ce709cc9c235d1f211b300c44
Health Tracker <syedmuazhassan@gmail.com>
2022-11-08 12:47:20 +0000

    - upto interim support

--- src/main/kotlin/ie/setu/App.kt
+++ src/main/kotlin/ie/setu/App.kt
@@ -2,7 +2,10 @@ package ie.setu
 import ie.setu.config.DbConfig
 import ie.setu.config.JavalinConfig
 
+
 fun main() {
+    // now on post interim work will be done
+
     DbConfig().getDbConnection()
     JavalinConfig().startJavalinService()
 }
